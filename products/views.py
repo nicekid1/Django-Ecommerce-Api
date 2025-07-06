8a0803d33c055c70da85e86e8376583365edb1b8
alimohtarami <alimohtarami5@gmail.com>
2025-07-06 20:59:20 +0330

    update views.py

--- products/views.py
+++ products/views.py
@@ -1,20 +1,23 @@
 from rest_framework import viewsets, permissions, filters
-from django_filters.rest_framework import DjangoFilterBackend
-from django.contrib.postgres.search import TrigramSimilarity
 from rest_framework.decorators import action
-from django.db.models import Value, CharField
-from django.db.models.functions import Cast
 from rest_framework.response import Response
 
-from .models import Product, Category, ProductImage
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Value, CharField
+from django.db.models.functions import Cast
+from django.contrib.postgres.search import TrigramSimilarity
+
+from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
 from .filters import ProductFilter
+
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user and request.user.is_authenticated and request.user.is_staff
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('-created_at')
@@ -33,7 +36,10 @@ class ProductViewSet(viewsets.ModelViewSet):
             return Response({'detail': 'No search query provided.'}, status=400)
 
         products = Product.objects.annotate(
-        similarity=TrigramSimilarity('name', Cast(Value(query), CharField()))
+            similarity=TrigramSimilarity(
+                Cast('name', CharField()),
+                Cast(Value(query), CharField())
+            )
         ).filter(similarity__gt=0.2).order_by('-similarity')
 
         serializer = self.get_serializer(products, many=True)
@@ -41,6 +47,6 @@ class ProductViewSet(viewsets.ModelViewSet):
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
-  queryset = Category.objects.all()
-  serializer_class = CategorySerializer
-  permission_classes = [IsAdminOrReadOnly]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]

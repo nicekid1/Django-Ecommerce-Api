0348c2e65c9a9a22273f456cbed06b0d3b18aa58
alimohtarami <alimohtarami5@gmail.com>
2025-07-06 19:02:37 +0330

    update serializers.py

--- products/serializers.py
+++ products/serializers.py
@@ -5,3 +5,17 @@ class ProductImageSerializer(serializers.ModelSerializer):
   class Meta:
     model = ProductImage
     fields = ['id','image']
+
+class CategorySerializer(serializers.ModelSerializer):
+  class Meta:
+    model:Category
+    fields = ['id', 'name', 'parent']
+
+class ProductSerializer(serializers.ModelSerializer):
+  images = ProductImageSerializer(many=True, read_only=True)
+  class Meta:
+        model = Product
+        fields = [
+            'id', 'name', 'description', 'price', 'stock', 'brand',
+            'color', 'size', 'weight', 'category', 'created_at', 'images'
+        ]

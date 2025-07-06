ee99db7c10ae45c017cb60411c103d710444dc23
alimohtarami <alimohtarami5@gmail.com>
2025-07-06 18:07:33 +0330

    update urls.cpython-313.pyc, urls.py and views.py

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets,permissions
 from .serializers import UserSerializer,RegisterSerializer
 from django.contrib.auth import get_user_model
 
-User = get_user_model
+User = get_user_model()
 
 class UserViewSet(viewsets.ModelViewSet):
   queryset = User.objects.all()

ed88659d9d663cba8d80fc35f4784775a68df78c
alimohtarami <alimohtarami5@gmail.com>
2025-07-06 18:06:19 +0330

    update urls.py

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet,RegisterViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
+router = DefaultRouter()
+router.register('register',RegisterViewSet,basename='register')
+router.register('profile',UserViewSet,basename='profile')
+
+urlpatterns = [
+    path('token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('',include(router))
+]

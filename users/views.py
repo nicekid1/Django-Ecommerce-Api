da15d9d3291b410c3dbfb5ce40b075ecbc79a36d
alimohtarami <alimohtarami5@gmail.com>
2025-07-06 17:50:48 +0330

    update views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets,permissions
+from .serializers import UserSerializer,RegisterSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model
+
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+  permission_classes = [permissions.IsAuthenticated]
+  

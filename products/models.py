5205290264f39f17872bd84b326ba9e68f906db9
alimohtarami <alimohtarami5@gmail.com>
2025-07-06 18:48:58 +0330

    update models.py

--- products/models.py
+++ products/models.py
@@ -17,3 +17,13 @@ class Product(models.Model):
     weight = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return f"Image for {self.product.name}"

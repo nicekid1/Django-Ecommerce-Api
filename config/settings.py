f5255985edee64f5dbe35b10baade3c56e44e846
alimohtarami <alimohtarami5@gmail.com>
2025-07-06 20:04:47 +0330

    update settings.cpython-313.pyc, settings.py and views.py

--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 from datetime import timedelta
--- products/views.py
+++ products/views.py
@@ -3,8 +3,10 @@ from .models import Product,Category,ProductImage
 from .serializers import ProductSerializer,CategorySerializer
 
 class IsAdminOrReadOnly(permissions.BasePermission):
-  def has_permission(self, request, view):
-    return request.method in permissions.SAFE_METHODS or request.user.is_staff
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_authenticated and request.user.is_staff
 
 class ProductViewSet(viewsets.ModelViewSet):
   queryset=Product.objects.all().order_by('-created_at')

65fa2ce29e27c9d7ba71bd7f5e2da842fd961c8b
alimohtarami <alimohtarami5@gmail.com>
2025-07-06 18:47:07 +0330

    update models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+  name =models.CharField(max_length=100)
+  parent = models.models.ForeignKey("self", null=True,blank=True, on_delete=models.CASCADE,related_name='children')
+  def __str__(self):
+    return self.name

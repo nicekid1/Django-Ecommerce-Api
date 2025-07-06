89ff0d9c477c3ddeca6497237e42e6ac1ca0a23e
alimohtarami <alimohtarami5@gmail.com>
2025-07-06 16:35:18 +0330

    update settings.cpython-313.pyc, settings.py and models.py

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    #our app
+    'users',
+
+    #rest
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +130,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User (AbstractUser):
+  isAdmin = models.BooleanField(default=False)
+  email_verified  = models.BooleanField(default=False)

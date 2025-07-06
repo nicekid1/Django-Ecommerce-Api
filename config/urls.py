494e38b0725e59be1e866084d60b1776eb51ddfb
alimohtarami <alimohtarami5@gmail.com>
2025-07-06 19:17:55 +0330

    update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/users/', include('users.urls')),
+    path('api/store/', include('products.urls')),
 ]

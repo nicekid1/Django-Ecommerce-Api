96f46074cf759bac23e9804bd9bc295cf2250a06
alimohtarami <alimohtarami5@gmail.com>
2025-07-07 18:28:01 +0330

    update models.py

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Category(models.Model):
   name =models.CharField(max_length=100)
@@ -27,3 +28,19 @@ class ProductImage(models.Model):
 
     def __str__(self):
         return f"Image for {self.product.name}"
+
+class CartItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='cart_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+      unique_together = ['user', 'product']
+    
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"    
+    
+    @property
+    def total_price(self):
+       return self.product.price * self.quantity

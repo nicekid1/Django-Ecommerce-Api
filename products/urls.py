6a24c584a4e72ab06ac7e63dab4e85ba174937c7
alimohtarami <alimohtarami5@gmail.com>
2025-07-16 19:48:06 +0330

    update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path , include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet,CategoryViewSet,CartViewSet,OrderViewSet,start_payment,verify_payment
+from .views import ProductViewSet,CategoryViewSet,CartViewSet,OrderViewSet,start_payment,verify_payment_view
 
 router = DefaultRouter()
 router.register('products',ProductViewSet, basename='products')
@@ -14,5 +14,5 @@ urlpatterns = [
 
 urlpatterns += [
     path('payment/start/<int:order_id>/', start_payment, name='start_payment'),
-    path('payment/verify/<int:order_id>/', verify_payment, name='verify_payment'),
+    path('payment/verify/<int:order_id>/', verify_payment_view, name='verify_payment'),
 ]

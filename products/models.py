6a11ff29018b5cf6aa4a4d2f1e34c07112966435
alimohtarami <alimohtarami5@gmail.com>
2025-07-17 18:56:14 +0330

    update models.py and views.py

--- products/models.py
+++ products/models.py
@@ -54,6 +54,7 @@ class Order(models.Model):
     ]
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders')
+    authority = models.CharField(max_length=50, blank=True, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

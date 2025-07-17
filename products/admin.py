ae67bb2f02a7af1236deeea22a9a4135236fa1bb
alimohtarami <alimohtarami5@gmail.com>
2025-07-17 19:24:19 +0330

    update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'stock')
+    list_filter = ('category',)
+    search_fields = ('title', 'description')
